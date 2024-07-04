bc1897cca4180e98f2782f8f5bcc137cbcf93812
mayromyller <deehmyller11@gmail.com>
2024-07-04 18:50:04 -0300

    feat: Add BoxCard component

---
+++ src/components/ui/layout/boxCard.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react'
+
+export function BoxCard({ children }: PropsWithChildren) {
+  return (
+    <div className="md:[box-shadow:0px_2px_14px_0px_rgba(0,0,0,0.2)] w-full">
+      {children}
+    </div>
+  )
+}
