796bd46e33b4b001d38b7f0d7838deb767b203c9
mayromyller <deehmyller11@gmail.com>
2024-07-04 18:54:51 -0300

    feat: Add new components and context to main app rendering

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,30 @@
+import { BoxCard, Carousel, CartContent, Header } from '@/components'
+import { SearchInput } from './components/ui/form/searchInput'
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <Header />
+
+      <div className="h-[3px] w-full bg-white" />
+
+      <main className="w-full bg-white md:bg-[#EEEEEE] min-h-screen pb-[35px]">
+        <div className="max-w-[1024px] mx-auto py-[6px] px-[16px] lg:px-0">
+          <SearchInput />
+        </div>
+
+        <div className="max-w-[1024px] lg:mx-auto md:bg-[#F8F9FA] bg-white md:px-[40px] px-[16px] md:py-8 py-5">
+          <div className="flex md:flex-row flex-col gap-6 items-start">
+            <BoxCard>
+              <div className="md:max-w-[600px] md:w-[600px] w-full">
+                <Carousel />
+              </div>
+            </BoxCard>
+
+            <CartContent />
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app.tsx'
 import './index.css'
+import { WebSettingsProvider } from '@/context'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <WebSettingsProvider>
+        <App />
+      </WebSettingsProvider>
+    </Provider>
   </React.StrictMode>
 )
