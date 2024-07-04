1e7cc6e4606ad16a47ac2d8d5a1b774b577b3e5f
mayromyller <deehmyller11@gmail.com>
2024-07-03 21:06:45 -0300

    feat: Add arrowIcon and searchIcon components

---
+++ src/components/icons/searchIcon.tsx
@@ -0,0 +1,18 @@
+export function SearchIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="20"
+      fill="none"
+      viewBox="0 0 21 20"
+    >
+      <path
+        fill="#8A94A4"
+        fillRule="evenodd"
+        d="M8.5 16a8 8 0 116.32-3.094l5.387 5.387-1.414 1.414-5.387-5.387A7.965 7.965 0 018.5 16zm6-8a6 6 0 11-12 0 6 6 0 0112 0z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  )
+}
