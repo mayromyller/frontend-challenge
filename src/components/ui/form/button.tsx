c87483ce167d5bfc7f90e3359eb7878639c47d89
mayromyller <deehmyller11@gmail.com>
2024-07-08 14:29:43 -0300

    style: Add disabled styling to button component

--- src/components/ui/form/button.tsx
+++ src/components/ui/form/button.tsx
@@ -19,7 +19,7 @@ export function Button({
     <button
       className={`${
         isFullWidth ? 'w-full' : ''
-      } bg-primary text-white font-bold py-2 px-4 rounded-full h-12`}
+      } bg-primary text-white font-bold py-2 px-4 rounded-full h-12 disabled:opacity-70 disabled:cursor-not-allowed`}
       style={{
         backgroundColor: bgColor
       }}
