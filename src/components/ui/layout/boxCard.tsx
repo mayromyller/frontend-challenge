cf16727b885fde348b045bae53f2cef8a1819d29
mayromyller <deehmyller11@gmail.com>
2024-07-09 11:30:06 -0300

    style: Update boxCard component styling

--- src/components/ui/layout/boxCard.tsx
+++ src/components/ui/layout/boxCard.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export function BoxCard({ children }: PropsWithChildren) {
   return (
-    <div className="md:[box-shadow:0px_2px_14px_0px_rgba(0,0,0,0.2)] w-full">
+    <div className="min-[900px]:[box-shadow:0px_2px_14px_0px_rgba(0,0,0,0.2)] w-full">
       {children}
     </div>
   )
