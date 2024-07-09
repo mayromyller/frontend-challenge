ab2da5ae76a2cab023ba49a3775246a5a2861373
mayromyller <deehmyller11@gmail.com>
2024-07-09 11:31:28 -0300

    feat: Add ShowCartButton component

---
+++ src/components/app/showCartButton.tsx
@@ -0,0 +1,27 @@
+import { useCartActions } from '@/store'
+import { Button } from '../ui/form/button'
+
+type ShowCartButtonProps = {
+  onClick?: () => void
+  label?: string | null
+}
+
+export function ShowCartButton({ label = null, onClick }: ShowCartButtonProps) {
+  const { totalInCart } = useCartActions()
+
+  const renderTotal = totalInCart() >= 1 ? totalInCart() : 0
+  const phraseToButton =
+    totalInCart() <= 1
+      ? `Your basket • ${renderTotal} item`
+      : `Your basket • ${renderTotal} items`
+
+  return (
+    <>
+      {totalInCart() >= 1 && (
+        <div className="hidden fixed bottom-0 left-0 right-0 z-40 max-[899px]:flex w-full h-20 items-center backdrop-blur-sm bg-white/30 px-6 pt-2">
+          <Button title={label ?? `${phraseToButton}`} onClick={onClick} />
+        </div>
+      )}
+    </>
+  )
+}
