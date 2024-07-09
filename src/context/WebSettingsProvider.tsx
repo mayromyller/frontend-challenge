1276faac1475f2ca5dc4593845641ccf74ab3e78
mayromyller <deehmyller11@gmail.com>
2024-07-09 17:31:41 -0300

    feat: Add currency type to WebSettingsContext

--- src/context/WebSettingsProvider.tsx
+++ src/context/WebSettingsProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react'
 
-import { WebSettingsType, getVenue } from '@/domain'
+import { VenueApi, WebSettingsType, getVenue } from '@/domain'
 
 type WebSettingsProps = Pick<
   WebSettingsType,
@@ -10,24 +10,39 @@ type WebSettingsProps = Pick<
   | 'primaryColour'
   | 'primaryColourHover'
 >
+type CurrencyType = Pick<VenueApi, 'currency' | 'locale'>
 
 interface WebSettingsContextProps {
   webSettings: WebSettingsProps
+  currencyType: CurrencyType
   isLoading: boolean
 }
 
 export const WebSettingsContext = createContext({} as WebSettingsContextProps)
 
 export function WebSettingsProvider({ children }: React.PropsWithChildren) {
-  const [webSettings, setWebSettings] = useState({} as WebSettingsProps)
+  const [webSettings, setWebSettings] = useState({
+    bannerImage: '',
+    backgroundColour: '#fff',
+    navBackgroundColour: '#111',
+    primaryColour: '#111',
+    primaryColourHover: '#111'
+  } as WebSettingsProps)
+
+  const [currencyType, setCurrencyType] = useState({
+    currency: 'BRL',
+    locale: 'pt-BR'
+  } as CurrencyType)
   const [isLoading, setIsLoading] = useState(true)
 
   async function fetchData() {
     try {
       const { data } = await getVenue(9)
       setWebSettings(data.webSettings)
-
-      setWebSettings(data.webSettings)
+      setCurrencyType({
+        currency: data.ccy,
+        locale: data.locale
+      })
     } catch (error) {
       console.log(error)
     } finally {
@@ -40,7 +55,9 @@ export function WebSettingsProvider({ children }: React.PropsWithChildren) {
   }, [])
 
   return (
-    <WebSettingsContext.Provider value={{ webSettings, isLoading }}>
+    <WebSettingsContext.Provider
+      value={{ webSettings, isLoading, currencyType }}
+    >
       {children}
     </WebSettingsContext.Provider>
   )
